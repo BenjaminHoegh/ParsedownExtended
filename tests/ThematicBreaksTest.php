5a9a27574717a2aad284f48beb9cde2caa979491
Benjamin Høegh <22152591+BenjaminHoegh@users.noreply.github.com>
2024-08-19 08:17:00 +0200

    Added unit test for Thematic breaks

--- tests/ThematicBreaksTest.php
+++ tests/ThematicBreaksTest.php
@@ -17,4 +17,27 @@ class ThematicBreaksTest extends TestCase
     {
         unset($this->parsedownExtended);
     }
+
+    public function testEnableThematicBreaks()
+    {
+        $this->parsedownExtended->config()->set('thematic_breaks', true);
+
+        $this->assertEquals("<hr />", $this->parsedownExtended->text("***"));
+    }
+
+    public function testDisableThematicBreaks()
+    {
+        $this->parsedownExtended->config()->set('thematic_breaks', false);
+
+        $this->assertEquals("<p>***</p>", $this->parsedownExtended->text("***"));
+    }
+
+    public function testDifferentThematicBreaks()
+    {
+        $this->parsedownExtended->config()->set('thematic_breaks', true);
+
+        $this->assertEquals("<hr />", $this->parsedownExtended->text("***"));
+        $this->assertEquals("<hr />", $this->parsedownExtended->text("---"));
+        $this->assertEquals("<hr />", $this->parsedownExtended->text("___"));
+    }
 }
