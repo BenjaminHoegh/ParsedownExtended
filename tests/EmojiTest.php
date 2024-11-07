5fc6364b62808eefa458cd2fb7c45607a5c468ec
Benjamin Høegh <22152591+BenjaminHoegh@users.noreply.github.com>
2024-11-07 23:19:55 +0100

    added more tests for emojis

--- tests/EmojiTest.php
+++ tests/EmojiTest.php
@@ -22,8 +22,8 @@ class EmojiTest extends TestCase
     {
         $this->parsedownExtended->config()->set('emojis', true);
 
-        $markdown = ":smile:";
-        $expectedHtml = '<p>😄</p>';
+        $markdown = ":grinning_face:";
+        $expectedHtml = '<p>😀</p>';
 
         $result = $this->parsedownExtended->text($markdown);
 
@@ -34,8 +34,54 @@ class EmojiTest extends TestCase
     {
         $this->parsedownExtended->config()->set('emojis', false);
 
-        $markdown = ":smile:";
-        $expectedHtml = '<p>:smile:</p>';
+        $markdown = ":grinning_face:";
+        $expectedHtml = '<p>:grinning_face:</p>';
+
+        $result = $this->parsedownExtended->text($markdown);
+
+        $this->assertEquals($expectedHtml, $result);
+    }
+
+    # Test that we dont parse emojis in code blocks
+    public function testEmojiInCodeBlock()
+    {
+        $this->parsedownExtended->config()->set('emojis', true);
+
+        $markdown = "```php\n:grinning_face:\n```";
+        $expectedHtml = '<pre><code class="language-php">:grinning_face:</code></pre>';
+
+        $result = $this->parsedownExtended->text($markdown);
+
+        $this->assertEquals($expectedHtml, $result);
+    }
+
+    # Test that we dont parse emojis in inline code
+    public function testEmojiInInlineCode()
+    {
+        $this->parsedownExtended->config()->set('emojis', true);
+
+        $markdown = "`:grinning_face:`";
+        $expectedHtml = '<p><code>:grinning_face:</code></p>';
+
+        $result = $this->parsedownExtended->text($markdown);
+
+        $this->assertEquals($expectedHtml, $result);
+    }
+
+    # Test that we dont parse emojis if the emoji code is inside a word (e.g. :grinning_face:ing or testing:grinning_face:)
+    public function testEmojiInWord()
+    {
+        $this->parsedownExtended->config()->set('emojis', true);
+
+        $markdown = "testing:grinning_face:";
+        $expectedHtml = '<p>testing:grinning_face:</p>';
+
+        $result = $this->parsedownExtended->text($markdown);
+
+        $this->assertEquals($expectedHtml, $result);
+
+        $markdown = ":grinning_face:ing";
+        $expectedHtml = '<p>:grinning_face:ing</p>';
 
         $result = $this->parsedownExtended->text($markdown);
 
